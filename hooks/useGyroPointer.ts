b6363d16d8830d1a72472409f92e0da153c8c01c
Dennis Woering <d.woering@hotmail.com>
2022-02-23 14:46:08 +0100

    fixes

--- hooks/useGyroPointer.ts
+++ hooks/useGyroPointer.ts
@@ -1,19 +1,46 @@
 import { useState, useEffect } from "react";
+import Quaternion from "quaternion";
 
 interface PointerCoordinates {
   x: number;
   y: number;
 }
 
+const rad = Math.PI / 180;
+
 const useGyroPointer = () => {
   const [pointerCoordinates, setPointerCoordinates] =
-    useState<PointerCoordinates>({x:0, y:0});
+    useState<PointerCoordinates>({ x: 0, y: 0 });
+  const [currentQuaternion, setCurrentQuaternion] = useState<Quaternion>(
+    Quaternion.fromEuler(0, 0, 0, "ZXY")
+  );
+  const [calibratedPoint, setCalibratedPoint] = useState<Quaternion>(
+    Quaternion.fromEuler(0, 0, 0, "ZXY")
+  );
+  const updateOrigin = () => {
+    setCalibratedPoint(currentQuaternion);
+  };
 
   function handleDeviceOrientation(event) {
     event.stopPropagation();
+    const currentRotation: Quaternion = Quaternion.fromEuler(
+      event.alpha * rad,
+      event.beta * rad,
+      event.gamma * rad,
+      "ZXY"
+    );
+
+    const angles = toEuler(currentRotation.toVector());
+
+    setCurrentQuaternion(currentRotation.toVector());
+    console.log(angles);
+
+    //let dist = euler.map((angle, i) => calcDist(angle, angle));
+
+
     setPointerCoordinates({
-      x: Math.min(Math.max(Math.floor(event.alpha), -45), 45),
-      y: Math.min(Math.max(Math.floor(event.beta), -45), 45),
+       x: angles[0],
+       y: angles[1],
     });
   }
 
@@ -27,5 +54,24 @@ const useGyroPointer = () => {
 
   return pointerCoordinates;
 };
+
+function toEuler(q) {
+  let sinr_cosp = 2 * (q[3] * q[0] + q[1] * q[2]);
+  let cosr_cosp = 1 - 2 * (q[0] * q[0] + q[1] * q[1]);
+  let roll = Math.atan2(sinr_cosp, cosr_cosp);
+
+  let siny_cosp = 2 * (q[3] * q[2] + q[0] * q[1]);
+  let cosy_cosp = 1 - 2 * (q[1] * q[1] + q[2] * q[2]);
+  let yaw = Math.atan2(siny_cosp, cosy_cosp);
+  return [yaw, roll];
+}
+
+function calcDist(angle, initAngle) {
+  angle = (angle - initAngle) * (180 / Math.PI);
+  angle = angle < 0 ? angle + 360 : angle;
+  angle = angle > 180 ? angle - 360 : angle;
+  let dist = Math.round(-800 * Math.tan(angle * (Math.PI / 180)));
+  return dist;
+}
 
 export default useGyroPointer;
--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import * as http from "http";
 import next, { NextApiHandler } from "next";
 import * as socketio from "socket.io";
-import roomManager from "lib/roomManager";
+//import roomManager from "lib/roomManager";
 
 const port: number = parseInt(process.env.PORT || "3000", 10);
 const dev: boolean = process.env.NODE_ENV !== "production";
@@ -24,24 +24,24 @@ nextApp.prepare().then(async () => {
     console.log("Client connected");
 
     socket.on("createroom", () => {
-      const room = roomManager.createNewRoom(socket.id);
-      socket.emit("createdroom", room.id);
-      socket.join(room.id);
-      console.log(`Created room with id ${room.id}`);
+      // const room = roomManager.createNewRoom(socket.id);
+      // socket.emit("createdroom", room.id);
+      // socket.join(room.id);
+      // console.log(`Created room with id ${room.id}`);
     });
 
     socket.on("joinroom", (id, onSuccessCallback, onErrorCallback) => {
-      if(!roomManager.rooms) {
-        onErrorCallback();
-      }
-      const room = roomManager.rooms.find(x => x.id == id);
-      if(!room) {
-        onErrorCallback();
-      }
+      // if(!roomManager.rooms) {
+      //   onErrorCallback();
+      // }
+      // const room = roomManager.rooms.find(x => x.id == id);
+      // if(!room) {
+      //   onErrorCallback();
+      // }
 
-      socket.join(room.id);
-      console.log(`Player joined room with id ${room.id}`);
-      onSuccessCallback();
+      // socket.join(room.id);
+      // console.log(`Player joined room with id ${room.id}`);
+      // onSuccessCallback();
     });
 
     socket.on("gyrovalues", value => {
