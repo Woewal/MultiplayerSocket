e7b597b03e9d76848e9c861e2089e15a214904bd
Dennis Woering <d.woering@hotmail.com>
2022-02-23 14:50:51 +0100

    fixes

--- lib/room.ts
+++ lib/room.ts
@@ -1,11 +1,11 @@
-// class Room {
-//     id : string;
-//     host : string;
+class Room {
+    id : string;
+    host : string;
 
-//     constructor(host : string) {
-//         this.host = host;
-//         this.id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 4).toUpperCase();
-//     }
-// }
+    constructor(host : string) {
+        this.host = host;
+        this.id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 4).toUpperCase();
+    }
+}
 
-// export default Room;
+export default Room;
--- lib/roomManager.ts
+++ lib/roomManager.ts
@@ -1,14 +1,13 @@
-// import Room from "./room";
+import Room from "./room";
 
-// class RoomManager {
-//     rooms : Room[] = [];
+class RoomManager {
+    rooms : Room[] = [];
 
-//     createNewRoom = (host:string) => {
-//         const room = new Room(host);
-//         this.rooms.push(room);
-//         return room;
-//     }
-// }
+    createNewRoom = (host:string) => {
+        const room = new Room(host);
+        this.rooms.push(room);
+        return room;
+    }
+}
 
-// export default new RoomManager();
-export {}
+export default new RoomManager();
